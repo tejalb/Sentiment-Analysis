678305d46fc13d0ce7297f3ab42cfb339ee83c30
tejalb <tbhamre@princeton.edu>
2014-07-07 21:06:56 -0700

    Create frequency.py
    Compute the term frequency histogram of the livestream data harvested in twitterstream.py

---
+++ frequency.py
@@ -0,0 +1,58 @@
+import sys
+import json
+
+def hw():
+    print 'Hello, world!'
+
+def lines(fp):
+    print str(len(fp.readlines()))
+
+def main():
+    #sent_file = open(sys.argv[1])
+    tweet_file = open(sys.argv[1])
+    #hw()
+    #lines(sent_file)
+    #lines(tweet_file)
+
+    content=[]
+    json_dict={}
+    tweet_file=open(sys.argv[1])
+#Why do I need to declare this again?? If I don't it doesn't read in
+    for line in tweet_file:
+     json_dict=json.loads(line)
+     if 'text' in json_dict.keys():
+      #print json_dict["text"]
+      unicode_str=json_dict['text']
+      encoded=unicode_str.encode('utf-8')
+      content.append(encoded)
+    
+    frequency={}
+    
+    #print content[2]
+    total_count=0
+    for tweet in content:
+     tweet=tweet.lower()
+     newtweet=tweet.replace('\n',"")
+     terms=newtweet.split(" ")
+     for term in terms:
+      total_count+=1
+      term=term.strip()
+      if term in frequency:
+        frequency[term]+=1
+      elif term not in frequency:
+        frequency[term]=1
+     
+        
+        
+    #print frequency.values()
+    #print total_count
+    for k in sorted(frequency, key=frequency.get, reverse=True):
+      print('%s %.6f' %(k, frequency[k]/total_count))
+
+    
+     
+    
+    
+
+if __name__ == '__main__':
+    main()
